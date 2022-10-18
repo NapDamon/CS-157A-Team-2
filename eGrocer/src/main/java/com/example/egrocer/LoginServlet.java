a1db5650098643f70d7940055de7bda78bda40f2
NapDamon <napdamon@gmail.com>
2022-10-18 14:10:59 -0700

    modified LoginServlet

--- eGrocer/src/main/java/com/example/egrocer/LoginServlet.java
+++ eGrocer/src/main/java/com/example/egrocer/LoginServlet.java
@@ -1,7 +1,7 @@
 package com.example.egrocer;
 
 import java.io.*;
-
+import java.sql.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -12,11 +12,19 @@ public class LoginServlet extends HttpServlet {
     public void init() {message = "Login Page";}
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = "";
+        String password = "";
         response.setContentType("text/html");
 
         //displaying Login text inputs and login page header
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<style>");
+
+        out.println("</style>");
+        out.println("</head>");
+        out.println("<body>");
         out.println("<h1>" + message + "</h1>");
         out.println("<br/>");
         out.println("<form>");
@@ -24,6 +32,13 @@ public class LoginServlet extends HttpServlet {
         out.println("<input type=\"text\" id=\"username\" name=\"username\"><br>");
         out.println("<label for=\"password\">Password:</label><br>");
         out.println("<input type=\"text\" id=\"password\" name=\"password\"><br>");
+        out.println("<br><input type=\"submit\" value=\"Login\"><br>");
+        username = request.getParameter("username");
+        password = request.getParameter("password");
         out.println("</body></html>");
+        out.close();
+    }
+
+    public void destroy() {
     }
 }
