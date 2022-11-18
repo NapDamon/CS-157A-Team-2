cfe837642f878a24019adcf370f4017f72a93041
nhingn <89226373+nhingn@users.noreply.github.com>
2022-11-17 19:41:42 -0800

    updated order summary

--- eGrocer/src/main/java/com/example/egrocer/OrderSummary.java
+++ eGrocer/src/main/java/com/example/egrocer/OrderSummary.java
@@ -18,11 +18,28 @@ public class OrderSummary extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         
         request.getRequestDispatcher("/WEB-INF/OrderSummary.jsp").forward(request,response);
-
+        
     }
    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String item = request.getParameter("item");
+        String subtotal = request.getParameter("subtotal");
+        String shipping = request.getParameter("shipping");
+        String tax = request.getParameter("tax");
+        String total = request.getParameter("total");
+         
+        OrderDetail orderDetail = new OrderDetail(item, subtotal, shipping, tax, total);
+ 
+        try {
+            PaymentServices paymentServices = new PaymentServices();
+            String approvalLink = paymentServices.authorizePayment(orderDetail);
+ 
+            response.sendRedirect(approvalLink);
+             
+        } catch (PayPalRESTException ex) {
+            ex.printStackTrace();
+            request.getRequestDispatcher("/WEB-INF/OrderSummary.jsp").forward(request,response);
+        }
 
-        request.getRequestDispatcher("/WEB-INF/OrderSummary.jsp").forward(request,response);
     }
 }
