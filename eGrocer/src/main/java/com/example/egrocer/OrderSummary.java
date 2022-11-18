9d36dd8eb0c8d64a215304c0948d990b7aeb31e3
nhingn <89226373+nhingn@users.noreply.github.com>
2022-11-17 18:38:14 -0800

    updated order summary

--- eGrocer/src/main/java/com/example/egrocer/OrderSummary.java
+++ eGrocer/src/main/java/com/example/egrocer/OrderSummary.java
@@ -6,23 +6,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "checkoutPage", value = "/checkoutPage")
+@WebServlet(name = "orderSummary", value = "/orderSummary")
 public class CheckOut extends HttpServlet{
 
     private String message;
 
     public void init() {
-        message = "Check Out Page";
+        message = "Order Summary";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         
-        request.getRequestDispatcher("/WEB-INF/CheckoutPage.jsp").forward(request,response);
+        request.getRequestDispatcher("/WEB-INF/OrderSummary.jsp").forward(request,response);
 
     }
    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/WEB-INF/CheckoutPage.jsp").forward(request,response);
+        request.getRequestDispatcher("/WEB-INF/OrderSummary.jsp").forward(request,response);
     }
 }
