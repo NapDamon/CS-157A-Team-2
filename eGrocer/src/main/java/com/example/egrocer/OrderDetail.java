3c52d940f5751fd7a07c60ef074c25c99c615bdf
nhingn <89226373+nhingn@users.noreply.github.com>
2022-11-17 19:42:08 -0800

    added order detail

---
+++ eGrocer/src/main/java/com/example/egrocer/OrderDetail.java
@@ -0,0 +1,35 @@
+public class OrderDetail {
+    private String itemName;
+    private float subtotal;
+    private float shipping;
+    private float tax;
+    private float total;
+ 
+    public OrderDetail(String itemName, String subtotal, String shipping, String tax, String total) {
+        this.itemName = itemName;
+        this.subtotal = Float.parseFloat(subtotal);
+        this.shipping = Float.parseFloat(shipping);
+        this.tax = Float.parseFloat(tax);
+        this.total = Float.parseFloat(total);
+    }
+ 
+    public String getItemName() {
+        return itemName;
+    }
+ 
+    public String getSubtotal() {
+        return String.format("%.2f", subtotal);
+    }
+ 
+    public String getShipping() {
+        return String.format("%.2f", shipping);
+    }
+ 
+    public String getTax() {
+        return String.format("%.2f", tax);
+    }
+     
+    public String getTotal() {
+        return String.format("%.2f", total);
+    }
+}
